1a82fd9837711add44e59882e88181a23d739085
DESKTOP-FCLA0CU\Su  Mon <e0046476@u.nus.edu>
2025-03-19 00:32:06 +0800

    SCRUM 127 : Retrieved retailer info during GetShoppingCartDetails

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrderManagement.Data;
+
+namespace OrderManagement.Repositories
+{
+    public class UserRepository :RepositoryBase<User>, IUserRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        public UserRepository(AppDbContext dbContextRepo) : base(dbContextRepo)
+        {
+            this.dbContext = dbContextRepo;
+        }
+
+        public async Task<Dictionary<Guid, User>> GetUserInfoByRetailerIdAsync(List<Guid> retailerIDs)
+        {
+            return await dbContext.Users
+                .Where(user => retailerIDs.Contains(user.UserID))
+                .ToDictionaryAsync(user => user.UserID, user => user);
+        }
+    }
+}
