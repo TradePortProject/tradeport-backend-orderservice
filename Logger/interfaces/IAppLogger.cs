3948c3ce9d08c2064900667a55f0c92e20e1445c
DESKTOP-FCLA0CU\Su  Mon <e0046476@u.nus.edu>
2025-04-06 16:52:09 +0800

    Split enum function to a new class (ENumHelper)

---
+++ Logger/interfaces/IAppLogger.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+namespace OrderManagement.Logger.interfaces
+{
+    public interface IAppLogger<T>
+    {
+        void LogInformation(string message, params object[] args);
+        void LogWarning(string message, params object[] args);
+        void LogError(Exception? exception, string message, params object[] args);
+        void LogError(string message);
+        void LogDebug(string message, params object[] args);
+    }
+}
